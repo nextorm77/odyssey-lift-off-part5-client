1af9a5b6a076a7c5d6d5c949f756c9d8f3393928
nextorm77 <sonsw77@gmail.com>
2022-09-05 22:31:05 +0900

    ready for deploying

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  // uri: 'http://localhost:4000',
+  uri: "https://svr-catstronats.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
